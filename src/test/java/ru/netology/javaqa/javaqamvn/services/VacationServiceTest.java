e4da7a6313e71094f0cdb2b2337d5eb62de28854
Mihail Test <misha.lucenko17@gmail.com>
2024-06-17 23:27:13 +0300

    Isparvlen commit

--- src/test/java/ru/netology/javaqa/javaqamvn/services/VacationServiceTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/VacationServiceTest.java
@@ -1,25 +1,27 @@
 package ru.netology.javaqa.javaqamvn.services;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
 
-    @Test
-    public void whenVacation1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/vacation.csv")
+    public void whenVacation(int income, int expenses, int threshold) {
         VacationService service = new VacationService();
 
-        int end = service.calculate(10000, 3000, 20000);
+        int end = service.calculate(income, expenses, threshold);
 
         System.out.println(end);
     }
 
-    @Test
-    public void whenVacation2() {
-        VacationService service = new VacationService();
-
-        int end = service.calculate(100_000, 60_000, 150_000);
-
-        System.out.println(end);
-    }
+//    @Test
+//    public void whenVacation2() {
+//        VacationService service = new VacationService();
+//
+//        int end = service.calculate(100_000, 60_000, 150_000);
+//
+//        System.out.println(end);
+//    }
 
 }
